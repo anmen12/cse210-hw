d0448e4c5415da7d0b3e6890282e33aa246e3ad6
anmen12 <balistricexurbalistrice@gmail.com>
2026-01-29 16:11:03 -0700

    Week03 Learning Activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction oneFraction = new Fraction();
+        Fraction fiveFraction = new Fraction(5);
+        Fraction threeFourthFraction = new Fraction(3,4);
+        Fraction oneThirdFraction = new Fraction(1,3);
+
+        Console.WriteLine($"{oneFraction.GetFractionString()}");
+        Console.WriteLine($"{oneFraction.GetDecimalValue()}");
+
+        Console.WriteLine($"{fiveFraction.GetFractionString()}");
+        Console.WriteLine($"{fiveFraction.GetDecimalValue()}");
+
+        Console.WriteLine($"{threeFourthFraction.GetFractionString()}");
+        Console.WriteLine($"{threeFourthFraction.GetDecimalValue()}");
+
+        Console.WriteLine($"{oneThirdFraction.GetFractionString()}");
+        Console.WriteLine($"{oneThirdFraction.GetDecimalValue()}");
     }
 }
