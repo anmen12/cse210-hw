bf75533e26fc420e4df74c3f8a151c8d26f8a470
anmen12 <balistricexurbalistrice@gmail.com>
2026-02-16 19:38:22 -0700

    Week06 Learning Activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("red", 6));
+        shapes.Add(new Rectangle("blue", 3, 4));
+        shapes.Add(new Circle("purple", 3));
+
+        foreach(Shape shape in shapes)
+        {
+            Console.WriteLine($"Color: {shape.GetColor()} Area: {shape.GetArea()}");
+        }
     }
 }
